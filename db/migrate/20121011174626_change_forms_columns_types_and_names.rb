5cd7d687dbec4b270b264aedf4e50f4c1e895970
Bryan Jenkins <bryancjenkins@gmail.com>
2012-11-29 17:40:49 -0500

    New attempt to fix Postgre problem

--- db/migrate/20121011174626_change_forms_columns_types_and_names.rb
+++ db/migrate/20121011174626_change_forms_columns_types_and_names.rb
@@ -1,33 +1,11 @@
 class ChangeFormsColumnsTypesAndNames < ActiveRecord::Migration
-  # def up
-  #   change_column :forms, :family_history, :integer
-  #   rename_column :forms, :family_history, :family_history_id
-  #   change_column :forms, :medical_history, :integer
-  #   rename_column :forms, :medical_history, :medical_history_id
-  # end
-
-  # def down
-  #   change_column :forms, :family_history, :text
-  #   rename_column :forms, :family_history_id, :family_history
-  #   change_column :forms, :medical_history, :text
-  #   rename_column :forms, :medical_history_id, :medical_history
-  # end
   def up
-    case ActiveRecord::Base.connection
-    when ActiveRecord::ConnectionAdapters::PostgreSQLAdapter
-        connection.execute(%q{
-            alter table forms
-            alter column family_history type integer using cast(family_history as integer),
-            rename column family_history to family_history_id
-        })
-    when ActiveRecord::ConnectionAdapters::MySQLAdapter
-        # MySQL version...
-        def up
-          change_column :forms, :family_history, :integer
-          rename_column :forms, :family_history, :family_history_id
-          change_column :forms, :medical_history, :integer
-          rename_column :forms, :medical_history, :medical_history_id
-        end
-    end
+    rename_column :forms, :family_history, :family_history_string
+    add_column :forms, :family_history_id, :integer
+    remove_column :forms, :family_history_string
+  end
+
+  def down
+    
   end
 end
