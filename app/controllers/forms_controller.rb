3e1869dcd38ee32381fc62574b47a13cf267445f
Bryan Jenkins <bryancjenkins@gmail.com>
2012-12-05 16:00:22 -0500

    Change hard coded user ID to work with heroku db

--- app/controllers/forms_controller.rb
+++ app/controllers/forms_controller.rb
@@ -7,7 +7,7 @@ class FormsController < ApplicationController
 
   def new
     # Hard coded to create only posts for Bryan
-    @user = User.find(1)
+    @user = User.find(2)
     @form = Form.new
     @form.medical_history = MedicalHistory.new
     @form.family_history = FamilyHistory.new
@@ -16,7 +16,7 @@ class FormsController < ApplicationController
 
   def create
     # Hard coded to create only posts for Bryan
-    @user = User.find(1)
+    @user = User.find(2)
     @form = @user.forms.build(params[:form])
     if @form.save 
       flash[:success] = "Record Saved Successfully!"
